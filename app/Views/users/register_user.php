0b75fad4eb9fd79524478b24580a96288f1fd58c
Big-Bud <webforce321@gmail.com>
2016-12-15 16:55:43 +0100

    form

--- app/Views/users/register_user.php
+++ app/Views/users/register_user.php
@@ -9,25 +9,32 @@
 					<!-- Si le champ est remplie aucune données entre en BDD -->
 					<input type="text" name="antiBot" value="" class="hide">
 					<div class="form-group">
-			    	<input type="text" class="form-control" name="firstname" placeholder="Nom">
+			    	<input type="text" class="form-control" name="firstname" placeholder="Nom" value="<?php if(!empty($_POST['firstname'])) {echo $_POST['firstname'];} ?>">
+						<span class="errorMessage"><?php if(!empty($error['firstname'])) { echo($error['firstname']);} ?></span>
 					</div>
 					<div class="form-group">
-						<input type="text" class="form-control" name="lastname" placeholder="Prenom">
+						<input type="text" class="form-control" name="lastname" placeholder="Prenom" value="<?php if(!empty($_POST['lastname'])) {echo $_POST['lastname'];} ?>">
+						<span class="errorMessage"><?php if(!empty($error['lastname'])) { echo($error['lastname']);} ?></span>
 					</div>
 					<div class="form-group">
-						<input type="text" class="form-control" name="username" placeholder="Pseudo">
+						<input type="text" class="form-control" name="username" placeholder="Pseudo" value="<?php if(!empty($_POST['username'])) {echo $_POST['username'];} ?>">
+						<span class="errorMessage"><?php if(!empty($error['username'])) { echo($error['username']);} ?></span>
 					</div>
 					<div class="form-group">
-						<input type="email" class="form-control" name="email" placeholder="Email">
+						<input type="email" class="form-control" name="email" placeholder="Email" value="<?php if(!empty($_POST['email'])) {echo $_POST['email'];} ?>">
+						<span class="errorMessage"><?php if(!empty($error['email'])) { echo($error['email']);} ?></span>
 					</div>
 					<div class="form-group">
 						<input type="password" class="form-control" name="password" placeholder="Mot de passe">
+						<span class="errorMessage"><?php if(!empty($error['password'])) { echo($error['password']);} ?></span>
 					</div>
 					<div class="form-group">
 						<input type="password" class="form-control" name="password_confirm" placeholder="Veuillez confirmer votre mot de passe">
+						<span class="errorMessage"><?php if(!empty($error['password_confirm'])) { echo($error['password_confirm']);} ?></span>
 					</div>
 					<div class="form-group">
-						<label><input type="checkbox" value="">J'accepte les <a href="<?php echo $this->url('cgu'); ?>">CGU</a></label>
+						<label><input type="checkbox" name="checkbox" value="">J'accepte les <a href="<?php echo $this->url('cgu'); ?>">CGU</a></label>
+						<span class="errorMessage"><br><?php if(!empty($error['checkbox'])) { echo($error['checkbox']);} ?></span>
 					</div>
 					<input type="submit" class="btn btn-default">
 				</div>
