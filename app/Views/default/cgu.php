a7396d2a161c336e0612a9ac11a16ef6c1118ac2
Kilian Tatain Mirail <tatain1@hotmail.com>
2016-12-26 15:22:23 +0100

    Mise en forme des CGU

--- app/Views/default/cgu.php
+++ app/Views/default/cgu.php
@@ -1,73 +1,101 @@
 <?php $this->layout('layout', ['title' => 'CGU']) ?>
 
 <?php $this->start('main_content') ?>
-	<h2>Let's code.</h2>
-	<p>Vous avez atteint la page de CGU. Bravo.</p>
-
+	<h2>CGU</h2>
 	<p>
 		Le présent document a pour objet de définir les modalités et conditions dans lesquelles nous mettons, d’une part à disposition des utilisateurs le site, et les services disponibles sur celui-ci et d’autre part, la manière par laquelle l’utilisateur accède au site et utilise ses services.
-
+	</p>
+	<p>
 		Toute connexion au site est subordonnée au respect des présentes conditions.
-
+	</p>
+	<p>
 		Pour l’utilisateur, le simple accès au site implique l’acceptation de l’ensemble des conditions décrites ci-après.
-
-		Propriété intellectuelle
-
+	</p>
+	<h3>
+		Propriété intellectuelle :
+	</h3>
+	<p>
 		La structure générale du site a-Swap , ainsi que les textes, graphiques, images, sons et  vidéos la composant, sont la propriété de ses développeurs ou de leurs partenaires. Toute représentation et/ou reproduction et/ou exploitation partielle ou totale des contenus et services proposés par le site a-Swap , par quelque procédé que ce soit, sans l'autorisation préalable et par écrit de  l'équipe en charge du développement est strictement interdite et serait susceptible de constituer une contrefaçon au sens des articles L 335-2 et suivants du Code de la propriété intellectuelle.
-
-		Liens hypertextes
-
+	</p>
+	<h2>
+		Liens hypertextes :
+	</h2>
+	<p>
 		Le site  a-Swap  peut contenir des liens hypertextes vers d’autres sites présents sur le réseau Internet. Les liens vers ces autres ressources vous font quitter le site a-Swap
-
+	</p>
+	<p>
 		Il est possible de créer un lien vers la page de présentation de ce site sans autorisation expresse des développeurs. Aucune autorisation ou demande d’information préalable ne peut être exigée par les développeurs à l’égard d’un site qui souhaite établir un lien vers le site. Il convient toutefois d’afficher ce site dans une nouvelle fenêtre du navigateur. Cependant, l'équipe de développement se réserve le droit de demander la suppression d’un lien qu’il estime non conforme à l’objet du site a-Swap.
-
+	</p>
+	<h2>
 		Responsabilité des développeurs :
-
+	</h2>
+	<p>
 		Les informations et/ou documents figurant sur ce site et/ou accessibles par ce site proviennent de sources considérées comme étant fiables.
-
+	</p>
+	<p>
 		Toutefois, ces informations et/ou documents sont susceptibles de contenir des inexactitudes techniques et des erreurs typographiques.
-
+	</p>
+	<p>
 		L'équipe de développement se réserve le droit de les corriger, dès que ces erreurs sont portées à sa connaissance.
-
+	</p>
+	<p>
 		Il est fortement recommandé de vérifier l’exactitude et la pertinence des informations et/ou documents mis à disposition sur ce site.
-
+	</p>
+	<p>
 		Les informations et/ou documents disponibles sur ce site sont susceptibles d’être modifiés à tout moment, et peuvent avoir fait l’objet de mises à jour. En particulier, ils peuvent avoir fait l’objet d’une mise à jour entre le moment de leur téléchargement et celui où l’utilisateur en prend connaissance.
-
+	</p>
+	<p>
 		L’utilisation des informations et/ou documents disponibles sur ce site se fait sous l’entière et seule responsabilité de l’utilisateur, qui assume la totalité des conséquences pouvant en découler, sans que l’équipe de développement puisse être recherché à ce titre, et sans recours contre ce dernier.
-
+	</p>
+	<p>
 		L’équipe de développement ne pourra en aucun cas être tenu responsable de tout dommage de quelque nature qu’il soit résultant de l’interprétation ou de l’utilisation des informations et/ou documents disponibles sur ce site.
-
+	</p>
+	<h2>
 		Accès au site
-
+	</h2>
+	<p>
 		L'équipe de développement ne peut garantir une disponibilité du site et/ou des services, une fiabilité des transmissions et des performances en terme de temps de réponse ou de qualité. Il n’est prévu aucune assistance technique vis à vis de l’utilisateur que ce soit par des moyens électronique ou téléphonique.
-
+	</p>
+	<p>
 		La responsabilité de l’équipe de développement ne saurait être engagée en cas d’impossibilité d’accès à ce site et/ou d’utilisation des services.
-
+	</p>
+	<p>
 		Par ailleurs, les développeurs peuvent être amené à interrompre le site ou une partie des services, à tout moment sans préavis, le tout sans droit à indemnités. L’utilisateur reconnaît et accepte que l’équipe de développement ne soit pas responsable des interruptions, et des conséquences qui peuvent en découler pour l’utilisateur ou tout tiers.
-
+	</p>
+	<h2>
 		Modification des conditions d’utilisation
-
+	</h2>
+	<p>
 		Les développeurs se réservent la possibilité de modifier, à tout moment et sans préavis, les présentes conditions d’utilisation afin de les adapter aux évolutions du site et/ou de son exploitation.
-
+	</p>
+	<h2>
 		Règles d'usage d'Internet
-
+	</h2>
+	<p>
 		L’utilisateur déclare accepter les caractéristiques et les limites d’Internet, et notamment reconnaît que :
-
+	</p>
+	<p>
 		L’équipe de développement n’assume aucune responsabilité sur les services accessibles par Internet et n’exerce aucun contrôle de quelque forme que ce soit sur la nature et les caractéristiques des données qui pourraient transiter par l’intermédiaire de son centre serveur.
-
+	</p>
+	<p>
 		L’utilisateur reconnaît que les données circulant sur Internet ne sont pas protégées notamment contre les détournements éventuels. La présence du logo   a-Swap  institue une présomption simple de validité. La communication de toute information jugée par l’utilisateur de nature sensible ou confidentielle se fait à ses risques et périls.
-
+	</p>
+	<p>
 		L’utilisateur reconnaît que les données circulant sur Internet peuvent être réglementées en termes d’usage ou être protégées par un droit de propriété.
-
+	</p>
+	<p>
 		L’utilisateur est seul responsable de l’usage des données qu’il consulte, interroge et transfère sur Internet.
-
+	</p>
+	<p>
 		L’utilisateur reconnaît que l’équipe de développement ne dispose d’aucun moyen de contrôle sur le contenu des services accessibles sur Internet
-
+	</p>
+	<h2>
 		Droit applicable
-
+	</h2>
+	<p>
 		Tant le présent site que les modalités et conditions de son utilisation sont régis par le droit français, quel que soit le lieu d’utilisation. En cas de contestation éventuelle, et après l’échec de toute tentative de recherche d’une solution amiable, les tribunaux français seront seuls compétents pour connaître de ce litige.
-
+	</p>
+	<p>
 		Pour toute question relative aux présentes conditions d’utilisation du site, vous pouvez nous contacter via le formulaire présent sur le site.
-
 	</p>
 <?php $this->stop('main_content') ?>
