2c9bfab556f3b678b59de21ed54cff04eda37906
tatain1 <tatain1@hotmail.com>
2017-01-03 17:01:16 +0100

    Correction dans les routes

--- app/routes.php
+++ app/routes.php
@@ -5,49 +5,49 @@
 		['GET', '/', 'Default#home', 'default_home'],
 
 		// contact
-		['GET', '/contact', 'Default#contact', 'contact'],
-		['POST', '/contact', 'Default#sendMailContact', 'send_mail_contact'],
+		['GET', '/contact/', 'Default#contact', 'contact'],
+		['POST', '/contact/', 'Default#sendMailContact', 'send_mail_contact'],
 
 		// profil
-		['GET', '/profil', 'User#profil', 'profil'],
-		['POST', '/profil', 'User#updateProfil', 'update_profil'],
-		['POST', '/profil/img', 'User#updateProfilImg', 'update_profil_img'],
+		['GET', '/profil/', 'User#profil', 'profil'],
+		['POST', '/profil/', 'User#updateProfil', 'update_profil'],
+		['POST', '/profil/img/', 'User#updateProfilImg', 'update_profil_img'],
 
 		// CGU
-		['GET', '/cgu', 'Default#cgu', 'cgu'],
+		['GET', '/cgu/', 'Default#cgu', 'cgu'],
 
 		// Deconnexion
-		['GET', '/deconnexion', 'User#deconnexion', 'deconnexion'],
+		['GET', '/deconnexion/', 'User#deconnexion', 'deconnexion'],
 
 		// Inscription User
 		['GET', '/inscription/user/[:token_asso]/[:token_invit]', 'User#registerUserFromInvite', 'register_user_from_invite'],
-		['GET', '/inscription/user', 'User#registerUser', 'register_user'],
-		['POST', '/inscription/user', 'User#tryRegister', 'try_register'],
+		['GET', '/inscription/user/', 'User#registerUser', 'register_user'],
+		['POST', '/inscription/user/', 'User#tryRegister', 'try_register'],
 
 		// Inscription Admin
-		['GET', '/inscription/admin_assos', 'UserAdmin#registerAdmin', 'admin_assos_register'],
-		['POST', '/inscription/admin_assos', 'UserAdmin#tryRegisterAdmin', 'admin_assos_try_register'],
+		['GET', '/inscription/admin_assos/', 'UserAdmin#registerAdmin', 'admin_assos_register'],
+		['POST', '/inscription/admin_assos/', 'UserAdmin#tryRegisterAdmin', 'admin_assos_try_register'],
 
 		// Forgot password
-		['GET', '/connexion/forgot_password', 'User#forgotPassword', 'forgot_password'],
-		['POST', '/connexion/forgot_password', 'User#tryForgotPassword', 'try_forgot_password'],
+		['GET', '/connexion/forgot_password/', 'User#forgotPassword', 'forgot_password'],
+		['POST', '/connexion/forgot_password/', 'User#tryForgotPassword', 'try_forgot_password'],
 
 		// Modify password
-		['GET', '/connexion/modify_password', 'User#modifyPassword', 'modify_password'],
-		['POST', '/connexion/modify_password', 'User#tryModifyPassword', 'try_modify_password'],
+		['GET', '/connexion/modify_password/', 'User#modifyPassword', 'modify_password'],
+		['POST', '/connexion/modify_password/', 'User#tryModifyPassword', 'try_modify_password'],
 
 		// Connexion
-		['GET', '/connexion', 'User#login', 'login'],
-		['POST', '/connexion', 'User#tryLogin', 'try_login'],
+		['GET', '/connexion/', 'User#login', 'login'],
+		['POST', '/connexion/', 'User#tryLogin', 'try_login'],
 
 
 
 
 		// Messagerie
-		['GET', '/messagerie', 'Message#message', 'message'],
-		['GET', '/messagerie/reception', 'Message#getMessage', 'get_message'],
-		['POST', '/messagerie', 'Message#sendMessage', 'send_message'], // surement des parametre a passer en URL, ne pas oublier de changer dans les $this->show concerné
-		['POST', '/messagerie/confirmation', 'Message#confirmAssosInvit', 'confirm_assos_invit'],
+		['GET', '/messagerie/', 'Message#message', 'message'],
+		['GET', '/messagerie/reception/', 'Message#getMessage', 'get_message'],
+		['POST', '/messagerie/', 'Message#sendMessage', 'send_message'], // surement des parametre a passer en URL, ne pas oublier de changer dans les $this->show concerné
+		['POST', '/messagerie/confirmation/', 'Message#confirmAssosInvit', 'confirm_assos_invit'],
 
 
 		// Admin Back
@@ -55,7 +55,7 @@
 		['GET', '/admin/back/[:slug]', 'UserAdmin#back', 'admin_back'],
 		// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 		['GET', '/admin/transaction/', 'TransactionAdmin#ShowFormTransaction', 'admin_back_transac'], //transaction de l'admin
-		['POST', '/admin/transaction', 'TransactionAdmin#makeTransactionAdmin', 'admin_back_transac_valid'], //transaction de l'admin
+		['POST', '/admin/transaction/', 'TransactionAdmin#makeTransactionAdmin', 'admin_back_transac_valid'], //transaction de l'admin
 		['GET', '/admin/deleteuser/[i:id]', 'AssociationAdmin#deleteUser', 'admin_back_delete'], // Delete user
 		['GET', '/admin/crediter/[i:id]', 'TransactionAdmin#ShowFormCredit', 'admin_back_credite'], //credite de l'admin
 		['POST', '/admin/crediter/', 'TransactionAdmin#makeCreditAdmin', 'admin_back_credite_valid'], //credite de l'admin
@@ -63,27 +63,27 @@
 		// Admin Association Back
 		['GET', '/admin/back/association/[:slug]', 'AssociationAdmin#backAssos', 'admin_back_assos'],
 		['GET', '/admin/back/association/modification/[:slug]', 'AssociationAdmin#backAssosModif', 'admin_back_modifassos'],
-		['POST', '/admin/back/association/modification', 'AssociationAdmin#backAssosTryModif', 'admin_back_assos_trymodif'],
-		['POST', '/admin/back/association', 'AssociationAdmin#addCoinToUser', 'admin_back_assos_addcoinuser'],
+		['POST', '/admin/back/association/modification/', 'AssociationAdmin#backAssosTryModif', 'admin_back_assos_trymodif'],
+		['POST', '/admin/back/association/', 'AssociationAdmin#addCoinToUser', 'admin_back_assos_addcoinuser'],
 
 		// Page accueil users connecté
 		['GET', '/accueil/[:slug]', 'User#usersAccueil', 'users_accueil'], // Afficher la page d'accueil du user avec liste des adhérants et bouton transaction
-		['GET', '/transaction', 'Transaction#ShowFormTransaction', 'users_accueil_transac'], // Page de transaction, formulaire
-		['POST', '/transaction', 'Transaction#makeTransactionUser', 'users_accueil_transac_valid'], // Post de la transaction user
+		['GET', '/transaction/', 'Transaction#ShowFormTransaction', 'users_accueil_transac'], // Page de transaction, formulaire
+		['POST', '/transaction/', 'Transaction#makeTransactionUser', 'users_accueil_transac_valid'], // Post de la transaction user
 
 		// Formulaire pour mettre à jour son assos (changer logo, texte...)
 		['GET', '/admin/association/update/[i:id]', 'AssociationAdmin#updateform', 'admin_association_update_form'],
 		['POST', '/admin/association/update/[i:id]', 'AssociationAdmin#updateaction', 'admin_association_update_action'],
 
 		// Formulaire pour inviter un nouveau membre
-		['GET', '/admin/back/invite', 'UserAdmin#back', 'admin_association_invite'],
-		['POST', '/admin/back/invite', 'AssociationAdmin#inviteNewMemberByMail', 'admin_association_invite_action'],
+		['GET', '/admin/back/invite/', 'UserAdmin#back', 'admin_association_invite'],
+		['POST', '/admin/back/invite/', 'AssociationAdmin#inviteNewMemberByMail', 'admin_association_invite_action'],
 
 		// Action pour accepter une invitation reçu en MP
 		['GET', '/acceptinvitation/[:token_asso]/[:token_invit]', 'Invitation#accept', 'accept_invitation'],
 
 		//Super Admin Back
-		['GET', '/superadmin/back', 'SuperAdmin#superAccueil', 'super_admin_back'],
+		['GET', '/superadmin/back/', 'SuperAdmin#superAccueil', 'super_admin_back'],
 		// Super Admin route user
 		// super Admin route asso
 		// Super Admin route messagerie.
