595a99cc3b87c67b1da1c1187bc73507b7bf4652
Big-Bud <webforce321@gmail.com>
2017-01-04 12:36:18 +0100

    truc

--- app/Views/layout_admin_back.php
+++ app/Views/layout_admin_back.php
@@ -10,27 +10,30 @@
 <body>
 		<header>
 			<nav class="navbar">
-				<div class="container-fluid">
-					<div class="navbar-header">
-						<div class="navbar-default">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-								<span class="sr-only">Toggle navigation</span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-							</button>
+				<div class="wrap">
+					<div class="container-fluid">
+						<div class="navbar-header">
+							<div class="navbar-default">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+						<a href="<?php echo $this->url('default_home') ?>"><img src="<?php echo $this->assetUrl('img/logo.png') ?>" class="logo"></a>
 						</div>
-					<a href="<?php echo $this->url('default_home') ?>"><img src="<?php echo $this->assetUrl('img/logo.png') ?>" class="logo"></a>
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="<?php echo $this->url('admin_back', ['slug' => $this->e($slug), 'page'=>1]) ?>">Transactions</a></li>
+								<li><a href="<?php echo $this->url('admin_back_assos', ['slug' => $this->e($slug)]) ?>">Gestion</a></li>
+								<li><a href="<?php echo $this->url('users_accueil', ['slug' => $this->e($slug)]) ?>">Front Office</a></li>
+								<li><a href="<?php echo $this->url('admin_back_modifassos', ['slug' => $this->e($slug)]) ?>"><?php echo $_SESSION['user']['nom_assos'] ?></a></li> <!-- possibilité de modifier l'assos -->
+								<li><a href="<?= $this->url('deconnexion') ?>">Deconnexion</a><li>
+							</ul>
+						</div>
 					</div>
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						<ul class="nav navbar-nav navbar-right">
-							<li><a href="<?php echo $this->url('admin_back', ['slug' => $this->e($slug), 'page'=>1]) ?>">Transactions</a></li>
-							<li><a href="<?php echo $this->url('admin_back_assos', ['slug' => $this->e($slug)]) ?>">Gestion</a></li>
-							<li><a href="<?php echo $this->url('users_accueil', ['slug' => $this->e($slug)]) ?>">Front Office</a></li>
-							<li><a href="<?php echo $this->url('admin_back_modifassos', ['slug' => $this->e($slug)]) ?>"><?php echo $_SESSION['user']['nom_assos'] ?></a></li> <!-- possibilité de modifier l'assos -->
-							<li><a href="<?= $this->url('deconnexion') ?>">Deconnexion</a><li>
-						</ul>
-					</div>
+					<div class="extension_head"></div>
 				</div>
 			</nav>
 		</header>
