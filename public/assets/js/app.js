99d810fe4883181649caa325239a3fd5404c53bb
Big-Bud <webforce321@gmail.com>
2017-01-09 09:45:07 +0100

    K2000

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -14,25 +14,22 @@
 // ============================================================================
 //                            FORM MATERIAL DESING
 // ============================================================================
-  jQuery(function($) {
-    //  Au focus
-    $('.field-input').focus(function(){
-      $(this).parent().addClass('is-focused has-label formbar');
-    });
+jQuery(function($) {
+  $('.field-input').focus(function(){
+    $(this).parent().addClass('is-focused has-label');
+  });
 
-    // à la perte du focus
-    $('.field-input').blur(function(){
-      $parent = $(this).parent();
-      if($(this).val() == ''){
-        $parent.removeClass('is-focused');
-      }
-      $parent.removeClass('formbar');
-    });
+  $('.field-input').each(function(){
+    if($(this).val() != ''){
+      $(this).parent().addClass('has-label');
+    }
+  });
 
-    // si un champs est rempli on rajoute directement la class has-label
-    $('.field-input').each(function(){
-      if($(this).val() != ''){
-        $(this).parent().addClass('has-label is-focused');
-      }
-    });
-  })
+  $('.field-input').blur(function(){
+    $parent = $(this).parent();
+    if($(this).val() == ''){
+      $parent.removeClass('has-label');
+    }
+    $parent.removeClass('is-focused');
+  });
+})
