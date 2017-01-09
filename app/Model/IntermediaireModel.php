1d56f3496710d62cc5547aba2f2955e4037d4b0e
Oshiiro <benjamin.delaborde@laposte.net>
2017-01-09 15:17:51 +0100

    Securisation transaction

--- app/Model/IntermediaireModel.php
+++ app/Model/IntermediaireModel.php
@@ -131,7 +131,8 @@ class IntermediaireModel extends UModel
     $sth->bindValue(':id_user', $id_user);
     $sth->execute();
     $id_asso_user = $sth->fetch();
-
+    debug($id_asso_user);
+    die();
     if($id_asso_admin == $id_asso_user) {
       return true;
     } else {
